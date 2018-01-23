d691d104ac806cc9883fbc85c260dc504b5cf31e
lk <s0556670@htw-berlin.de>
2018-01-23 15:15:29 +0100

    Version_final

--- temp_funktionen.py
+++ temp_funktionen.py
@@ -32,19 +32,45 @@ def k_to_c(x):
         return x + ABSOLUTER_NP_C
     else:
         raise TypeError(FEHLERMELDUNG_TEMP)
-
+def k_to_f(x):
+    if x >= ABSOLUTER_NP_K:
+        return x * 1.8 - 241.15
+    else:
+        raise TypeError(FEHLERMELDUNG_TEMP)
+def f_to_c(x):
+    if x >= ABSOLUTER_NP_F:
+        return (x - NULL_F) / 1.8
+    else:
+        raise TypeError(FEHLERMELDUNG_TEMP)
+def f_to_k(x):
+    if x >= ABSOlUTER_NP_F:
+        return (x - ABSOLUTER_NP_F) / 1.8
+    else:
+        raise TypeError(FEHLERMELDUNG_TEMP)
 wahl = -1
-wahl = int(input("Bitte waehlen: "))
+while wahl != 0:
+    wahl = int(input("Bitte waehlen: "))
 
 
-if wahl == 1:
-    x = get_float("Temperatur in Celsius eingeben: ")
-    print (x, "Grad =",  c_to_k(x), "Kelvin")   
-elif wahl == 2:
-    x = get_float("Temperatur in Celsius eingeben: ")
-    print (x, "Grad = ", c_to_f(x), "Fahrenheit")
-elif wahl == 3:
-    x = get_float("Temperatur in Kevlin eingeben: ")
-    print (x, "Kelvin ", k_to_c(x), "Grad")
-    
+    if wahl == 1:
+        x = get_float("Temperatur in Celsius eingeben: ")
+        print (x, "Grad =",  c_to_k(x), "Kelvin")   
+    elif wahl == 2:
+        x = get_float("Temperatur in Celsius eingeben: ")
+        print (x, "Grad =", c_to_f(x), "Fahrenheit")
+    elif wahl == 3:
+        x = get_float("Temperatur in Kevlin eingeben: ")
+        print (x, "Kelvin = ", k_to_c(x), "Grad")
+    elif wahl == 4:
+        x = get_float("Temperatur in Kevlin eingeben: ")
+        print (x, "Kelvin =", k_to_f(x), "Fahrenheit")
+    elif wahl == 5:
+        x = get_float("Temperatur in Fahrenheit eingeben: ")
+        print(x, "Fahrenheit =", f_to_c(x), "Celsius")
+    elif wahl == 6:
+        x = get_float("Temperatur in Fahrenheit eingeben: ")
+        print(x, "Fahrenheit =", f_to_k(x), "Kevlin")
+    else:
+        print("Programm wurde vom Nutzer beendet.")
+        break
 
